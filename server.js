68ffffdf3b444006f611b8230878a612524a854a
janani1995 <cutesuja24@gmail.com>
2017-11-02 15:51:38 +0530

    change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
    password : '',
-   database : 'reportcardcloud'
+   database : 'reportcardnew'
 });
 
 
